3cfe342ad9d34e51cb6513964607c5434cf25408
Tony <tonylo@ossii.com.tw>
2012-09-11 14:26:13 +0800

    1 commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index');
+};
+exports.page = function(req, res){
+  res.render('page');
+};
+exports.menubar = function(req, res){
+  res.render('menubar');
+};
+exports.login = function(req, res){
+  res.render('login');
+};
